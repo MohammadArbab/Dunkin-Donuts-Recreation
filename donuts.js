00538d9c29f9baca7ea1fa3bec6e5264f7d4b5d6
MohammadArbab <44480064+MohammadArbab@users.noreply.github.com>
2019-07-12 15:28:58 -0400

    Add files via upload

---
+++ donuts.js
@@ -0,0 +1,6 @@
+function revealMessage() {
+    document.getElementById("hidden").style.display = 'flex';
+    document.getElementById("hidden").style.justifyContent = 'center';
+    document.getElementById("hidden").style.color = 'orange';
+    document.getElementById("hidden").style.textDecoration = 'bold';
+}
